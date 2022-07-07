ff5573a3810d11cd8075cd6a839cf6bcd1059134
oussamahorrigue <horrigueouss@gmail.com>
2022-07-07 10:47:31 +0200

    change package name

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export { default as ApproveHandLikeSvgrepoCom } from "./ApproveHandLikeSvgrepoCo
 export { default as AudioMediaMultimedia38SvgrepoCom } from "./AudioMediaMultimedia38SvgrepoCom";
 export { default as CameraHipsterOnTrendSvgrepoCom } from "./CameraHipsterOnTrendSvgrepoCom";
 export { default as CloudComputingData2SvgrepoCom } from "./CloudComputingData2SvgrepoCom";
+export { default as HeadphonesHipsterMusicSvgrepoCom } from "./HeadphonesHipsterMusicSvgrepoCom";
 export { default as HeartLoveRomanticSvgrepoCom } from "./HeartLoveRomanticSvgrepoCom";
 export { default as MagnifierOutPlusSvgrepoCom } from "./MagnifierOutPlusSvgrepoCom";
 export { default as Mp3MusicNextSvgrepoCom } from "./Mp3MusicNextSvgrepoCom";
