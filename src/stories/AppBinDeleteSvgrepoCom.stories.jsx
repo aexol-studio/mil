765a09e049a1a758b3ed9e01e557e3fdb8d103d5
Oussama <Oussama@Julianas-Mac-mini.local>
2022-07-11 14:17:34 +0200

    create stories

---
+++ src/stories/AppBinDeleteSvgrepoCom.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SvgAppBinDeleteSvgrepoCom from '../AppBinDeleteSvgrepoCom'
+
+export default {
+  title: 'SvgAppBinDeleteSvgrepoCom',
+  component: SvgAppBinDeleteSvgrepoCom,
+}
+
+const Template = () => <SvgAppBinDeleteSvgrepoCom />
+
+export const Icon = Template.bind({})
---
+++ src/stories/HeartLoveRomanticSvgrepoCom.stories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import SvgHeartLoveRomanticSvgrepoCom from '../HeartLoveRomanticSvgrepoCom'
+
+export default {
+  title: 'Example/SvgHeartLoveRomanticSvgrepoCom',
+  component: SvgHeartLoveRomanticSvgrepoCom,
+}
+
+const Template = () => (
+  <SvgHeartLoveRomanticSvgrepoCom title={undefined} titleId={undefined} />
+)
+
+export const Icon = Template.bind({})
