4cea4d8c3656100ab43ca003bfb4607196870b14
Oussama <Oussama@Julianas-Mac-mini.local>
2022-07-18 11:05:14 +0200

    add icon

---
+++ src/stories/SvgCallContactMultimediaSvgrepoCom.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import SvgCallContactMultimediaSvgrepoCom from '../CallContactMultimediaSvgrepoCom'
+
+export default {
+  title: 'SvgCallContactMultimediaSvgrepoCom',
+  component: SvgCallContactMultimediaSvgrepoCom,
+}
+
+const Template = () => <SvgCallContactMultimediaSvgrepoCom />
+
+export const Icon = Template.bind({})
